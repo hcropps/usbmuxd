97eb040d17cc00a163361156ed1532b0b2b69ec8
hcropps <112782115+hcropps@users.noreply.github.com>
2022-09-27 23:55:08 +0300

    Update conf.c

--- src/conf.c
+++ src/conf.c
@@ -128,7 +128,12 @@ const char *config_get_config_dir()
 #ifdef __APPLE__
 	base_config_dir = strdup("/var/db");
 #else
-	base_config_dir = strdup("/var/lib");
+	
+	#ifdef __ANDROID__
+		base_config_dir = strdup("/data/data/com.mtn.move.to.ios.watransfer/files/lib");
+	#else
+		base_config_dir = strdup("/var/lib");
+	#endif
 #endif
 #endif
 	__config_dir = string_concat(base_config_dir, DIR_SEP_S, CONFIG_DIR, NULL);
